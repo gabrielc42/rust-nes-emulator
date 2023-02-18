6b402e8a849297aa79d8a81e230476ca1d469746
gabrielc42 <clark.gabe97@gmail.com>
2023-02-18 04:52:05 -0600

    memory addressing nodes init

--- nes/src/cpu.rs
+++ nes/src/cpu.rs
@@ -3,9 +3,40 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF],
 }
 
 impl CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.run()
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.program_counter = 0x8000;
+    }
+
+    pub fn run(&mut self) {
+        // move initialization of program_counter from here to load fn
+        loop {
+            let opscode = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+
+            match opscode {
+                // ..
+            }
+        }
+    }
+
     pub fn new() -> Self {
         CPU {
             register_a: 0,
