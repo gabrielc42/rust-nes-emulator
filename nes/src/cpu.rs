aa5e375788497da1396578e5cf2eae71109efb36
gabrielc42 <clark.gabe97@gmail.com>
2023-02-24 05:03:53 -0600

    test_5_ops_working_together

--- nes/src/cpu.rs
+++ nes/src/cpu.rs
@@ -859,6 +859,15 @@ mod test {
     }
 
     #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert.eq!(cpu.register_x, 0xc1)
+    }
+
+    
+    #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
